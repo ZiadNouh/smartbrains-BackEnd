d67347de38233841189ae395e3c35d2deef539f2
ziad <ziad.nouh111@gmail.com>
2023-09-06 00:43:09 +0300

    using code from chatgpt

--- controllers/image.js
+++ controllers/image.js
@@ -1,14 +1,26 @@
 const handleImage = (req, res, db) => {
   const { id } = req.body;
+
+  // Ensure that id is a valid integer
+  if (!Number.isInteger(id)) {
+    return res.status(400).json("Invalid id");
+  }
+
+  // Update the "entries" column for the user with the specified ID
   db("users")
     .where("id", "=", id)
     .increment("entries", 1)
     .returning("entries")
     .then((entries) => {
-      res.json(entries[0].entries);
+      // Send the updated entry count as a response
+      res.json(entries[0]);
     })
-    .catch((err) => res.status(400).json("unable to get entries"));
+    .catch((err) => {
+      console.error(err); // Log the error for debugging
+      res.status(500).json("Internal server error"); // Handle the error gracefully
+    });
 };
+
 module.exports = {
   handleImage,
 };
